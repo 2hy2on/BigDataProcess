1f8959bd2925296ab02069e0161452b13f5276f3
2hy2on <mashin0128@gmail.com>
2023-11-07 13:10:46 +0900

    movie

---
+++ HW2/IMDBStudent20210793.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+fr = open(inputFile, "rt")
+fw = open(outputFile, "wt")
+
+print(inputFile)
+
+genre = dict()
+while True:
+    row = fr.readline()
+    if not row: break
+    rowSplit = row.split("::")
+    temp = rowSplit[2]
+    gen = temp.split('|')
+    for g in gen:
+        g = g.replace("\n", "")
+        if g not in genre:
+            genre[g] = 1
+        else:
+            genre[g] += 1
+
+fr.close()
+for key, value in genre.items():
+    fw.write(f'{key}: {value}\n')
+fw.close()
+
+
