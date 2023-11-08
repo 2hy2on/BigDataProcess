d007fbe6c71f5a75fbef11a83d2b105533f7de49
2hy2on <mashin0128@gmail.com>
2023-11-08 16:36:02 +0900

    정렬 수정

--- HW2/UBERStudent20210793.py
+++ HW2/UBERStudent20210793.py
@@ -7,7 +7,7 @@ outputFile = sys.argv[2]
 
 fr = open(inputFile, "rt")
 fw = open(outputFile, "wt")
-
+result = []
 week = ["MON", "TUE", "WEB", "THU", "FRI", "SAT", "SUN"]
 while True:
     row = fr.readline()
@@ -16,17 +16,28 @@ while True:
     date = rowSplit[1]
     dateSplit = date.split("/")
     week_num = datetime(int(dateSplit[2]),int(dateSplit[0]), int(dateSplit[1])).weekday()
-    rowSplit[1] = week[week_num]
+    rowSplit[1] = str(week_num)
 
-    s = ""
+    s = []
     for i in range(len(rowSplit)):
-        s += rowSplit[i]
+        rowSplit[i] = rowSplit[i].replace("\n", "")
+        s.append(rowSplit[i])
         if i == 0 or i == 2:
-            s += ","
+            s.append(",")
         if i == 1:
-            s+=" "
-    fw.write(s+"\n")
+            s.append(" ")
+    result.append(s)
+
+result.sort(key=lambda x: [x[0],x[2]])
 fr.close()    
+
+for r in result:
+    for i in range(len(r)):
+        if i == 2:
+            fw.write(week[int(r[i])])
+        else:
+            fw.write(r[i])
+    fw.write("\n")
 fw.close()
 
     
